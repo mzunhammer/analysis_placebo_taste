dfdcf3049d2ba0e9ae0c96a92970ec24ec94beec
Unknown <matthias.zunhammer@uk-essen.de>
2019-06-13 13:56:24 +0200

    Added main

--- A0_MAIN.m
+++ A0_MAIN.m
@@ -4,7 +4,5 @@ run('A2_Import_Heart_Rate_Data.m')
 run('A3_Extract_Heart_Rate_Data.m')
 run('A4_Create_wide_df.m')
 
-
-run('A1_Import_Data.m')
-run('A4_Create_wide_df.m')
-run('Export_For_Max.m')
+% Run all analyses
+run('B_Analysis.m')
